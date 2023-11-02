cb0fb83caa503091ae36240b9e1ea8fb01ce383b
Arif Hossain <hossaininfo@ymail.com>
2023-11-02 20:11:32 +0100

    handled error, loading and styled

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -7,26 +7,51 @@ import {useNavigate} from "react-router-dom";
 const Blogs = () => {
 
     const [posts, setPosts] = useState([]);
-    let navigate = useNavigate();
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
     const fetchPosts = async () => {
+        setIsLoading(true);
+        setError(null);
         try {
             const response = await getPosts();
-            // console.log(response.data);
             setPosts(response.data);
         } catch (error) {
             console.log(error);
+            setError("FAILED_TO_LOAD_POSTS")
+        }finally {
+            setIsLoading(false);
         }
     }
 
     useEffect(() => {
+
         fetchPosts();
     }, []);
 
     const handleShowBlogs = (postId) => {
         navigate(`/blog/${postId}`);
     }
+
+    if (isLoading) {
+        return (
+            <div className={styles.loading}>
+                <div className={styles.spinner}></div>
+                <p>Loading posts...</p>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className={styles.error}>
+                <p>Sorry, we could not load the posts. Please try again later.</p>
+                <button onClick={fetchPosts}>Retry</button>
+            </div>
+        );
+    }
+
 
     return (
         <div className={styles.container}>
@@ -35,14 +60,18 @@ const Blogs = () => {
                 <h4>Discover your purpose in life; find something that makes you want to wake up each day because your
                     purpose fuels you!</h4>
                 <div className={styles.blog_card}>
-                    {posts.map((post) => (
-                        <BlogCard
-                            key={post.id}
-                            post={post}
-                            onClick={() => handleShowBlogs(post.id)}
-                        />
+                    {posts.length > 0 ? (
+                        posts.map((post) => (
+                            <BlogCard
+                                key={post.id}
+                                post={post}
+                                onClick={() => handleShowBlogs(post.id)}
+                            />
 
-                    ))}
+                        ))
+                    ):(
+                        <p>No blog available.</p>
+                    )}
                 </div>
             </div>
         </div>
